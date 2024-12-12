fb6cff97a7d60dadd24bb0e1f0da1432e83ddbd2
clt <446075873@qq.com>
2024-12-12 15:25:19 +0800

    feat(app): 添加 PDF 阅读器组件
    - 在首页引入 MyPdfReader 组件
    - 新增 MyPdfReader 组件实现 PDF 文件阅读功能
    - 使用 react-pdf 库加载和渲染 PDF 文件
    - 添加页面切换和显示当前页码的功能

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import MyPdfReader from "@/components/MyPdfReader";
 import TicTocGame from "@/components/TicTocGame/index";
 
 export default function Home() {
   return (
     <>
       <TicTocGame />
+      <MyPdfReader />
     </>
   );
 }
---
+++ src/components/MyPdfReader/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url,
+).toString();
+
+import { useState } from "react";
+import { Document, Page } from "react-pdf";
+
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+export default function Home() {
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document
+        file="/assets/test-react-pdf.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+}
